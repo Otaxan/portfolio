ef1b82e7c5a26123b7a2c796bfad8b031db68300
Otaxan <109284777+Otaxan@users.noreply.github.com>
2023-02-05 00:08:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let h1=document.getElementById('h1')
+let h2=document.getElementById('h2')
+let input=document.getElementById('input')
+let button=document.getElementById('button')
+
+
+
+
+// let text = input.value
+function butto() {
+    h2.innerText=input.value
+    input.value=''
+}
